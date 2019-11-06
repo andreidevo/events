016601cd8e7296fafb828ac2f67d862fa5b1ce2c
YorkIsMine <vladal56hack@yandex.ru>
2019-11-06 21:59:51 +0300

    ALL UPDATED!

--- app/src/main/java/com/r/events/view/MainActivity.kt
+++ app/src/main/java/com/r/events/view/MainActivity.kt
@@ -2,14 +2,15 @@ package com.r.events.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.viewpager.widget.ViewPager
-import com.fxn.BubbleTabBar
-import com.fxn.OnBubbleClickListener
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener
 import com.r.events.R
-import com.r.events.ViewModel
-import com.r.events.model.PagerAdapt
+import com.r.events.ScreenSlidePageFragment
+import com.r.events.ScreenSlidePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,20 +18,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //создаем связь
-        val viewModel = ViewModel()
+        val fragList = ArrayList<ScreenSlidePageFragment>()
+        fragList.add(
+            ScreenSlidePageFragment.newInstance(
+                getString(R.string.main_page),
+                R.color.white
+            )
+        )
+        fragList.add(
+            ScreenSlidePageFragment.newInstance(
+                getString(R.string.mapped_list),
+                R.color.colorOrange
+            )
+        )
+        fragList.add(
+            ScreenSlidePageFragment.newInstance(
+                getString(R.string.options),
+                R.color.white
+            )
+        )
+        fragList.add(
+            ScreenSlidePageFragment.newInstance(
+                getString(R.string.user),
+                R.color.colorOrange
+            )
+        )
 
-        //передаем контекст в модель
-        viewModel.getContext(this)
+        val pagerAdapter = ScreenSlidePagerAdapter(fragList, supportFragmentManager)
 
-        //меняем текст
-        val text = findViewById<TextView>(R.id.text)
-        viewModel.editText(text, "Мероприятия")
 
-        //отправим рандомное уведомление
-        //viewModel.pushNotification("1 окт 2019", "Программирование", null)
-        viewModel.getDataFromPage()
 
+
+        view_pager.setAdapter(pagerAdapter)
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(i: Int, v: Float, i1: Int) {
+
+            }
+
+            override fun onPageSelected(i: Int) {
+                bottom_navigation_view_linear.setCurrentActiveItem(i)
+            }
+
+            override fun onPageScrollStateChanged(i: Int) {
+
+
+            }
+
+        })
+
+        bottom_navigation_view_linear.setNavigationChangeListener(BubbleNavigationChangeListener { view, position ->
+            view_pager.setCurrentItem(
+                position,
+                true
+            )
+        })
 
     }
 }
