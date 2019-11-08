4c7a044e31e2d77292ba81ccd8498016163d3fc6
YorkIsMine <vladal56hack@yandex.ru>
2019-11-08 14:59:00 +0300

    Some changes

--- app/src/main/java/com/r/events/view/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/r/events/view/ui/home/HomeViewModel.kt
@@ -1,9 +1,31 @@
 package com.r.events.view.ui.home
 
+import android.content.Context
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.r.events.MainViewModel
+import com.r.events.adapter.EventAdapter
+import com.r.events.model.list
 
-
+//ToDo сделать так, чтобы context не нужно было постоянно ложить в каждый метод
 class HomeViewModel : ViewModel(){
 
+    fun setupRecyclerView(context: Context, recyclerView: RecyclerView): Unit{
+        val eventAdapter: EventAdapter = EventAdapter(context, list)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = eventAdapter
+    }
 
+    fun setupButton(btn: Button, fragment: Fragment, context: Context): Unit{
+        btn.setOnClickListener{
+            val mainViewModel =
+                ViewModelProviders.of(fragment).get(MainViewModel::class.java)
+            mainViewModel.getContext(context)
+            mainViewModel.getDataFromPage()
+        }
+    }
 }
