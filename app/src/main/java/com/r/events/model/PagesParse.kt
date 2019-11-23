c4693ef809be24b641575a28716b2971dbb58a28
Yaroslaww1 <yaroslaww1@gmail.com>
2019-11-23 21:12:00 +0200

    revorked EventObject

--- app/src/main/java/com/r/events/model/PagesParse.kt
+++ app/src/main/java/com/r/events/model/PagesParse.kt
@@ -247,64 +247,6 @@ class PagesParse {
         } catch (e: IOException) { }
     }
 
-    fun getDataNormal(len: Int, format: Int, data : ArrayList<ArrayList<Int>>): String {
-        val RUS: Int = 0
-        val ENG: Int = 1
-
-        val NUM_FORMAT: Int = 0 // 1-15-2019
-        val TEXT_FORMAN: Int = 1 // 1 Nov 2019
-        if (len == RUS) {
-            if (data.size == 2) {
-                //значит промежуток
-                if (format == NUM_FORMAT)
-                    return "от ${data[0][0]}-${data[0][1]}-${data[0][2]} до ${data[1][0]}-${data[1][1]}-${data[1][2]}"
-                else if (format == TEXT_FORMAN) {
-                    val firstMonth = Utils.convertNumToMonth(data[0][1], len)
-                    val secMonth = Utils.convertNumToMonth(data[1][1], len)
-                    val string = "от ${data[0][0]} ${firstMonth} ${data[0][2]} до ${data[1][0]} ${secMonth} ${data[1][2]}"
-                    Log.d("TAG", string)
-                    return string
-                }
-            }
-            else {
-                if (format == NUM_FORMAT)
-                    return "${data[0][0]}-${data[0][1]}-${data[0][2]}"
-                else if (format == TEXT_FORMAN) {
-                    val FirstMonth = Utils.convertNumToMonth(data[0][1], len)
-                    val string = "${data[0][0]} ${FirstMonth} ${data[0][2]}"
-                    Log.d("TAG", string)
-                    return string
-                }
-            }
-        }
-        else if (len == ENG) {
-            if (data.size == 2) {
-                //значит промежуток
-                if (format == NUM_FORMAT)
-                    return "${data[0][0]}-${data[0][1]}-${data[0][2]} for ${data[1][0]}-${data[1][1]}-${data[1][2]}"
-                else if (format == TEXT_FORMAN) {
-                    val firstMonth = Utils.convertNumToMonth(data[0][1], len)
-                    val secMonth = Utils.convertNumToMonth(data[1][1], len)
-                    val string = "${data[0][0]} ${firstMonth} ${data[0][2]} for ${data[1][0]} ${secMonth} ${data[1][2]}"
-                    return string
-                }
-                else
-                    return ""
-            }
-            else {
-                if (format == NUM_FORMAT)
-                    return "${data[0][0]}-${data[0][1]}-${data[0][2]}"
-                else if (format == TEXT_FORMAN) {
-                    val FirstMonth = Utils.convertNumToMonth(data[0][1], len) //ToDo тут заменить на английский перевод
-                    val string = "${data[0][0]} ${FirstMonth} ${data[0][2]}"
-                    return string
-                }
-            }
-        }
-
-        return ""
-    }
-
     /*
     fun mergeSort(list : ArrayList<EventObject>) : ArrayList<EventObject>
     {
